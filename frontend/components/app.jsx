6c845f5946618b070337f2ebe201ec9d88488f7f
Adi Kantawala <adikan@ucla.edu>
2018-12-13 09:51:19 -0800

    Minor update

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -22,7 +22,7 @@ const App = () => (
 
     <AuthRoute exact path="/login" component={LogInFormContainer} />
     <AuthRoute exact path="/signup" component={SignUpFormContainer} />
-
+    
   </div>
 );
 
