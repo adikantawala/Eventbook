a154d2051f612c82b31403ccee9eb4f334324efc
Adi Kantawala <adikan@ucla.edu>
2018-12-18 17:51:03 -0800

    Update seed file for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,9 @@ User.create(email: "demo@user.com", first_name: "Demo", last_name: "User", passw
 
 Event.delete_all
 
-Event.create(event_date:"Sun, 16 Dec 2018 01:52:29 UTC +00:00", creator_id: 1, location: "LA", title: "Cool Event 1", description: "description test 1", category_id: 1)
-Event.create(event_date:"Sat, 15 Dec 2018 02:52:29 UTC +00:00", creator_id: 1, location: "SF", title: "Cool Event 2", description: "description test 2", category_id: 1)
-Event.create(event_date:"Fri, 14 Dec 2018 03:52:29 UTC +00:00", creator_id: 1, location: "NY", title: "Cool Event 3", description: "description test 3", category_id: 1)
-Event.create(event_date:"Thurs, 12 Dec 2018 03:52:29 UTC +00:00", creator_id: 1, location: "LA", title: "Cool Event 4", description: "description test 4", category_id: 1)
-Event.create(event_date:"Wed, 11 Dec 2018 03:52:29 UTC +00:00", creator_id: 1, location: "SF", title: "Cool Event 5", description: "description test 5", category_id: 1)
-Event.create(event_date:"Tues, 10 Dec 2018 03:52:29 UTC +00:00", creator_id: 1, location: "NY", title: "Cool Event 6", description: "description test 6", category_id: 1)
+Event.create(event_date: Date.new(2018,12,22), creator_id: 1, location: "LA", title: "Cool Event 1", description: "description test 1", category_id: 1)
+Event.create(event_date: Date.new(2018,12,23), creator_id: 1, location: "SF", title: "Cool Event 2", description: "description test 2", category_id: 1)
+Event.create(event_date: Date.new(2018,12,24), creator_id: 1, location: "NY", title: "Cool Event 3", description: "description test 3", category_id: 1)
+Event.create(event_date: Date.new(2018,12,25), creator_id: 1, location: "LA", title: "Cool Event 4", description: "description test 4", category_id: 1)
+Event.create(event_date: Date.new(2018,12,26), creator_id: 1, location: "SF", title: "Cool Event 5", description: "description test 5", category_id: 1)
+Event.create(event_date: Date.new(2018,12,27), creator_id: 1, location: "NY", title: "Cool Event 6", description: "description test 6", category_id: 1)
